04d21e888c84454f4933a1dcec46a78551365308
urcoilbisurco <urcoilbisurco@gmail.com>
2017-02-27 10:53:07 +0100

    start using webpack2. On the way to remove aphrodite and using plain scss files for every component

---
+++ src/_env.js
@@ -0,0 +1,9 @@
+var env={
+  token:"2f0e7128e590e52b90182af2c7ae80ccb23ba245",
+  device:"7ab78fdaa113d5a611c8a669",
+  weather_token:"83ecbe0fabb4e2e23607541ce063417d"
+}
+//env.device="3d0032000947343337373738";
+module.exports=env;
+
+//darksky=83ecbe0fabb4e2e23607541ce063417d
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,72 @@
+
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/index.html',
+  template: __dirname + '/src/index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
+const path = require('path');
+const webpack = require('webpack');
 module.exports = {
-  entry: [
-    './app/index.js'
-  ],
-  output: {
-    path: __dirname + '/dist',
-    filename: "index_bundle.js"
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './app.js',
   },
-  module: {
-    loaders: [
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  module:{
+    rules:[
       {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-      {test: /\.scss$/, exclude: /node_modules/, loaders: ["style-loader", "css-loader", "sass-loader"]},
-      {test: /\.jpg$/, loader: "file-loader"}
-    ],
-    // rules: [{
-    //   test: /\.scss$/,
-    //   use: [{
-    //       loader: "style-loader" // creates style nodes from JS strings
-    //   }, {
-    //       loader: "css-loader" // translates CSS into CommonJS
-    //   }, {
-    //       loader: "sass-loader" // compiles Sass to CSS
-    //   }]
-    // }]
+      {test: /\.(jpg|png)$/, loader: "file-loader"},
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      }
+    ]
   },
   plugins: [HTMLWebpackPluginConfig]
 };
+
+
+
+// var HtmlWebpackPlugin = require('html-webpack-plugin')
+// var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+//   template: __dirname + '/app/index.html',
+//   filename: 'index.html',
+//   inject: 'body'
+// });
+//
+// module.exports = {
+//   entry: [
+//     './app/index.js'
+//   ],
+//   output: {
+//     path: __dirname + '/dist',
+//     filename: "index_bundle.js"
+//   },
+//   module: {
+//     loaders: [
+//       {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+//       {test: /\.scss$/, exclude: /node_modules/, loaders: ["style-loader", "css-loader", "sass-loader"]},
+//       {test: /\.jpg$/, loader: "file-loader"}
+//     ],
+//     // rules: [{
+//     //   test: /\.scss$/,
+//     //   use: [{
+//     //       loader: "style-loader" // creates style nodes from JS strings
+//     //   }, {
+//     //       loader: "css-loader" // translates CSS into CommonJS
+//     //   }, {
+//     //       loader: "sass-loader" // compiles Sass to CSS
+//     //   }]
+//     // }]
+//   },
+//   plugins: [HTMLWebpackPluginConfig]
+// };
